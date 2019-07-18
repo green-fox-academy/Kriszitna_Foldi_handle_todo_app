e8da171f7f4062078aca31688bfd14b3e2764ec6
KrisztinaFoldi <krisztifoldi11@gmail.com>
2019-07-18 14:54:13 +0200

    list task made w/ txt

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,23 +12,31 @@ public class Main {
       System.out.println("-l List all the tasks");
       System.out.println("-a Adds a new task");
       System.out.println("-r Removes an task");
-      System.out.println("-c Complets an task");
+      System.out.println("-c Completes an task");
     }
 
     if (args[0].equals("-l")){
-      listTasks();
-    }
-    }
-    public static void listTasks(){
-    List<String> tasks = new ArrayList<String>();
-    tasks.add("Walk the dog");
-    tasks.add("Buy milk");
-    tasks.add("Do homework");
-      for (int i = 1; i < tasks.size() + 1; i++) {
-        System.out.println(i + " - " + tasks.get(i-1));
+      try {
+        Path filePath = Paths.get("/Users/foldikrisztina/greenfox/Krisztina_Foldi_todo_app/src/list.txt");
+        List<String> lines = Files.readAllLines(filePath);
+        for (int i = 1 ; i < lines.size() + 1; i++) {
+          System.out.println(i + " - " + lines.get(i-1));
+        }
+      } catch (Exception e) {
+        System.out.println("can not read file");
       }
-
     }
+  }
+//    public static void listTasks(){
+//    List<String> tasks = new ArrayList<String>();
+//    tasks.add("Walk the dog");
+//    tasks.add("Buy milk");
+//    tasks.add("Do homework");
+//      for (int i = 1; i < tasks.size() + 1; i++) {
+//        System.out.println(i + " - " + tasks.get(i-1));
+//      }
+//
+//    }
 
 
 }
